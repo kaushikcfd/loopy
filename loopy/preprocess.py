32c284b366d7c53ced132247820d274a3069bc7b
Kaushik Kulkarni <kaushikcfd@gmail.com>
2021-07-06 09:45:51 -0500

    conserve the iname tags post realize reduction

--- loopy/preprocess.py
+++ loopy/preprocess.py
@@ -1984,7 +1984,8 @@ def realize_reduction_for_single_kernel(kernel, callables_table,
             temp_kernel = kernel.copy(
                     instructions=new_insns + insn_queue,
                     temporary_variables=new_temporary_variables,
-                    domains=make_loop_kernel_domains(domains))
+                    domains=make_loop_kernel_domains(domains,
+                                                     kernel.inames))
             temp_kernel = lp.replace_instruction_ids(
                     temp_kernel, insn_id_replacements)
             changed = True
@@ -1998,7 +1999,8 @@ def realize_reduction_for_single_kernel(kernel, callables_table,
         kernel = kernel.copy(
             instructions=new_insns,
             temporary_variables=new_temporary_variables,
-            domains=make_loop_kernel_domains(domains))
+            domains=make_loop_kernel_domains(domains,
+                                             kernel.inames))
 
     kernel = lp.replace_instruction_ids(kernel, insn_id_replacements)
 
