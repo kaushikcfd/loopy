760fcf3a4ec14d264a6afdbdf791283ad997ffad
Kaushik Kulkarni <kaushikcfd@gmail.com>
2021-11-14 15:38:29 -0600

    [precompute, perf.] Visit only relevant expressions

--- loopy/transform/precompute.py
+++ loopy/transform/precompute.py
@@ -24,16 +24,18 @@ THE SOFTWARE.
 import islpy as isl
 from loopy.symbolic import (get_dependencies,
         RuleAwareIdentityMapper, RuleAwareSubstitutionMapper,
-        SubstitutionRuleMappingContext)
+        SubstitutionRuleMappingContext, CombineMapper)
 from loopy.diagnostic import LoopyError
 from pymbolic.mapper.substitutor import make_subst_func
 from loopy.translation_unit import TranslationUnit
+from loopy.kernel.instruction import MultiAssignmentBase
 from loopy.kernel.function_interface import CallableKernel, ScalarCallable
 from loopy.kernel.tools import (kernel_has_global_barriers,
                                 find_most_recent_global_barrier)
 import numpy as np
 
 from pymbolic import var
+from pytools import memoize_on_first_arg
 
 from loopy.transform.array_buffer_map import (ArrayToBufferMap, NoOpArrayToBufferMap,
         AccessDescriptor)
@@ -244,6 +246,12 @@ class RuleInvocationReplacer(RuleAwareIdentityMapper):
         for insn in kernel.instructions:
             self.replaced_something = False
 
+            if (isinstance(insn, MultiAssignmentBase)
+                    and self.subst_name not in get_calls_in_insn(insn)):
+                # 'insn' does not have a call 'subst' => do not process
+                new_insns.append(insn)
+                continue
+
             insn = insn.with_transformed_expressions(
                     lambda expr: self(expr, kernel, insn))
 
@@ -276,6 +284,52 @@ class RuleInvocationReplacer(RuleAwareIdentityMapper):
 
 class _not_provided:  # noqa: N801
     pass
+
+
+class FunctionNameCollector(CombineMapper):
+    def combine(self, values):
+        from functools import reduce
+        return reduce(frozenset.union, values, frozenset())
+
+    def map_call(self, expr):
+        return self.combine([frozenset([expr.function.name])]
+                            + [self.rec(arg) for arg in expr.parameters])
+
+    def map_call_with_kwargs(self, expr):
+        return self.combine([frozenset([expr.function.name])]
+                            + [self.rec(arg) for arg in expr.parameters]
+                            + [self.rec(arg) for arg in expr.kw_parameters.values()])
+
+    def map_algebraic_leaf(self, expr):
+        return frozenset()
+
+    def map_constant(self, expr):
+        return frozenset()
+
+
+@memoize_on_first_arg
+def _get_calls_in_expr(expr):
+    return FunctionNameCollector()(expr)
+
+
+def get_calls_in_insn(insn):
+    assert isinstance(insn, MultiAssignmentBase)
+    from pymbolic.primitives import Expression
+    from functools import reduce
+    return ((_get_calls_in_expr(insn.expression)
+             if isinstance(insn.expression, Expression)
+             else frozenset())
+            | reduce(frozenset.union,
+                     (_get_calls_in_expr(pred)
+                      for pred in insn.predicates
+                      if isinstance(pred, Expression)),
+                     frozenset())
+            | reduce(frozenset.union,
+                     (_get_calls_in_expr(assignee)
+                      for assignee in insn.assignees
+                      if isinstance(assignee, Expression)),
+                     frozenset())
+            )
 
 
 def precompute_for_single_kernel(kernel, callables_table, subst_use,
@@ -558,7 +612,8 @@ def precompute_for_single_kernel(kernel, callables_table, subst_use,
 
         import loopy as lp
         for insn in kernel.instructions:
-            if isinstance(insn, lp.MultiAssignmentBase):
+            if (isinstance(insn, lp.MultiAssignmentBase)
+                    and subst_name in get_calls_in_insn(insn)):
                 for assignee in insn.assignees:
                     invg(assignee, kernel, insn)
                 invg(insn.expression, kernel, insn)
